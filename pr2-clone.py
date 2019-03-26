57ea3d428287c6830c22f5170c55349c09c211a3
AjaniBurgos <ajaniburgos@cse.unr.edu>
2019-03-26 15:25:26 -0700

    added creation of custom bridge

--- pr2-clone.py
+++ pr2-clone.py
@@ -5,7 +5,6 @@ import subprocess
 import xml.etree.ElementTree as ET
 import uuid
 
-
 prefix = sys.argv[1]
 
 #credits to https://gist.github.com/pklaus/9638536#gistcomment-2013303, I guess python doesn't have a random MAC generator so here's our next best option
@@ -19,13 +18,20 @@ def rand_mac():
         random.randint(0, 255)
         )
 
+#try to think about arg parsing
+
+vm_bridge = "pr2-" + prefix + "-br"
+subprocess.run(["ovs-vsctl", "add-br", vm_bridge])
+#create new ovs bridge `ovs-vsctl`
+
 vms = ("base", "pi", "c1", "c2")
 for vm in vms:
+    #generate new drive `truncate`
     #generating new fields for renaming in the .xml
+
     vm_name = prefix + "-" + vm
     vm_drive = "/var/lib/libvirt/images/" + prefix + "-" + vm + ".qcow2" #before doing this, still need to do system call to make new drive
     vm_uuid = uuid.uuid4()
-    vm_bridge = prefix + "-br"
     mac_int = rand_mac()
     if vm is "base" or vm is "pi":
         mac_ex = rand_mac()
@@ -33,5 +39,10 @@ for vm in vms:
     else:
         print(vm_name, vm_drive, vm_uuid, mac_int, vm_bridge)
 
+    #done with the uuid
     vm_xml = ET.parse("vms/" + vm + ".xml").getroot()
     vm_xml.find('uuid').text = vm_uuid #uuidgen
+
+
+
+#start parsing and replacing stuff, write it to /tmp/<name>.xml, remove when done
