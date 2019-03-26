5ce667ffcd2c3638a0333ada16b6243ac378d237
Jake Wheeler <jakewhlr@gmail.com>
2019-03-26 15:46:34 -0700

    added argparsing

--- pr2-clone.py
+++ pr2-clone.py
@@ -4,8 +4,12 @@ import random
 import subprocess
 import xml.etree.ElementTree as ET
 import uuid
+import argparse
 
-prefix = sys.argv[1]
+parser = argparse.ArgumentParser()
+parser.add_argument("prefix")
+args = parser.parse_args()
+print(args.prefix)
 
 #credits to https://gist.github.com/pklaus/9638536#gistcomment-2013303, I guess python doesn't have a random MAC generator so here's our next best option
 def rand_mac():
@@ -16,11 +20,11 @@ def rand_mac():
         random.randint(0, 255),
         random.randint(0, 255),
         random.randint(0, 255)
-        )
+    )
 
 #try to think about arg parsing
 
-vm_bridge = "pr2-" + prefix + "-br"
+vm_bridge = "pr2-" + args.prefix + "-br"
 subprocess.run(["ovs-vsctl", "add-br", vm_bridge])
 #create new ovs bridge `ovs-vsctl`
 
@@ -29,8 +33,8 @@ for vm in vms:
     #generate new drive `truncate`
     #generating new fields for renaming in the .xml
 
-    vm_name = prefix + "-" + vm
-    vm_drive = "/var/lib/libvirt/images/" + prefix + "-" + vm + ".qcow2" #before doing this, still need to do system call to make new drive
+    vm_name = args.prefix + "-" + vm
+    vm_drive = "/var/lib/libvirt/images/" + args.prefix + "-" + vm + ".qcow2" #before doing this, still need to do system call to make new drive
     vm_uuid = uuid.uuid4()
     mac_int = rand_mac()
     if vm is "base" or vm is "pi":
